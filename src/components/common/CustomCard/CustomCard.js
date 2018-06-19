0301903dbc7de0a51b38c607153efa9221415e32
Harshit Jain <hjain.tech@gmail.com>
2018-06-19 22:16:13 +0530

    Added logo to card

--- src/components/common/CustomCard/CustomCard.js
+++ src/components/common/CustomCard/CustomCard.js
@@ -3,8 +3,10 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
+import GFGImage from '../../../assets/GFG-logo.jpeg';
 import './CustomCard.css';
 
 
@@ -13,6 +15,12 @@ const CustomCard = (props) => {
 //TODO: Anchor tag is being used inside button because we needed the linked page to open in a new tab. See if we can improve this, otherwise work on styling it properly
     return (
         <Card raised="true" className="Card">
+            <CardMedia
+          className="media"
+          image={GFGImage}
+          title="GeeksForGeeks"
+        />
+
             <CardHeader
                 title={props.title}
                 subheader="September 14, 2016"
